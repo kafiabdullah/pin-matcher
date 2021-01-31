0bf4597dbba7f1fac750513da2f9a8177726c621
kafi <abdullahkafi996@gmail.com>
2021-02-01 01:23:45 +0600

    add submit button click message and complete the project

--- js/main.js
+++ js/main.js
@@ -44,12 +44,31 @@ function verifyPin() {
     const pin = document.getElementById('pin').value;
     const typedPin = document.getElementById('typed-pin').value;
     if (pin === typedPin) {
-        const correct = document.getElementById('correct-pin');
-        correct.style.display = "block";
+        // const correct = document.getElementById('correct-pin');
+        // correct.style.display = "block";
+
+        // const correct = document.getElementById('incorrect-pin');
+        // correct.style.display = "none";
+        displayMatchResult('block', 'none');
+        
     }
     else {
-        const correct = document.getElementById('incorrect-pin');
-        correct.style.display = "block";
+        // const correct = document.getElementById('correct-pin');
+        // correct.style.display = "none";
 
+
+        // const correct = document.getElementById('incorrect-pin');
+        // correct.style.display = "block";
+        displayMatchResult('none', 'block');
+        
     }
+}
+
+// display match result
+function displayMatchResult(correctStatus, incorrectStatus){
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = correctStatus;
+
+    const incorrect = document.getElementById('incorrect-pin');
+    incorrect.style.display = incorrectStatus;
 }
